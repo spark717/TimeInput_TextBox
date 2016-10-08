8c3401a96acf3829ac9b1cc2535849c46db15888
Ruslan <Ruslan@BUMBLEBEE>
2016-10-08 13:19:10 +0300

    first checkpoint

--- InputTest/MainWindow.xaml.cs
+++ InputTest/MainWindow.xaml.cs
@@ -18,17 +18,19 @@ using System.Diagnostics;
 namespace InputTest
 {
     /// <summary>
-    /// Логика взаимодействия для MainWindow.xaml
+    /// Provide custom time input.
     /// </summary>
     public partial class MainWindow : Window
     {
-        private string _oldText = "__:__";
+        private string _oldText;
+        private string _template = "__:__";
 
         public MainWindow()
         {
             InitializeComponent();
 
-            InputBox.Text = "__:__";
+            InputBox.Text = _template;
+            _oldText = _template;
 
             InputBox.TextChanged += OnTextChanged;
         }
@@ -36,7 +38,7 @@ namespace InputTest
 
         private void OnTextChanged (object sender, TextChangedEventArgs e)
         {
-            InputBox.TextChanged -= OnTextChanged;
+            InputBox.TextChanged -= OnTextChanged;      //Remove this event
 
             int carretPos = InputBox.CaretIndex;
             StringBuilder text = new StringBuilder(InputBox.Text);
@@ -45,7 +47,7 @@ namespace InputTest
             int insertedNum;
             bool isNumber = Int32.TryParse(insertedChar, out insertedNum);
 
-            if (carretPos > 5 || !isNumber)
+            if (carretPos > _template.Length || !isNumber)     //If carret in last pos OR inserted char not number, then abort
             {
                 InputBox.Text = _oldText;
                 InputBox.CaretIndex = carretPos - 1;
@@ -54,24 +56,24 @@ namespace InputTest
                 return;
             }
 
-            if (text[carretPos].ToString() == ":")
+            if (text[carretPos].ToString() == ":")      //If carret right above ':' char, then remove inserted char and place him after
             {
                 text.Remove(carretPos - 1, 3);
                 text.Insert(carretPos - 1, ":" + insertedChar);
 
                 carretPos ++;
             }
-            else
+            else    //Standart behavior of program
             {
                 text.Remove(carretPos, 1);                
             }
 
-            InputBox.Text = text.ToString();
+            InputBox.Text = text.ToString();    //Replace old string with new one
             InputBox.CaretIndex = carretPos;
 
-            _oldText = InputBox.Text;
+            _oldText = InputBox.Text;       //Mem current standing
 
-            InputBox.TextChanged += OnTextChanged;
+            InputBox.TextChanged += OnTextChanged;      //Return event
         }
     }
 }
